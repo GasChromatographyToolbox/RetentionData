a81d0a6df5d6a7f741609e18e52cf7670395f15d
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2022-09-29 18:15:52 +0200

    Update test.jl

--- test/test.jl
+++ test/test.jl
@@ -1,6 +1,6 @@
 # development of tests for runtests.jl
 
 db_path = string(@__DIR__, "/Databases/Blumberg2017")
-csv_paths = ThermodynamicData.collect_csv_paths(db_path)
+csv_paths = RetentionData.collect_csv_paths(db_path)
 #@test
 string(db_path, "/Blumberg2017_Parameters_TableS1a_SLB5ms_beta250.csv") in csv_paths
