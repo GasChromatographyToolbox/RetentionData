42bef12c582d5e66fa480a864d28e9b85a985ba2
Jan Leppert <68964801+JanLeppert@users.noreply.github.com>
2023-02-23 09:42:20 +0100

    add documentation

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using Documenter
+using RetentionData
+
+makedocs(
+    sitename = "RetentionData",
+    #format = Documenter.HTML(),
+    #modules = [RetentionData]
+    pages = Any[
+                "Home" => "index.md",
+                "Docstrings" => "docstrings.md"
+            ]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+deploydocs(
+    repo = "github.com/JanLeppert/RetentionData",
+    devbranch = "main"
+)
---
+++ docs/src/index.md
@@ -0,0 +1,5 @@
+# RetentionData.jl
+
+Documentation for RetentionData.jl
+
+[RetentionData.jl](github.com/JanLeppert/RetentionData)
